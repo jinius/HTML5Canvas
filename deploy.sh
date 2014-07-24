1d895866c30b19c520300f47d9928bc46e71a400
Lee JinWoo <jinus.kr@gmail.com>
2014-07-25 00:01:10 +0900

    cache manifest

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
+appcache="cache.appcache";
 version=`date "+%Y-%m-%d_%H%M"`;
+server="jinius@ivorytower.co.kr";
 
 ################################
-# cache.manifest
+# cache manifest
 ################################
-cat > cache.manifest <<EOF
+cat > $appcache <<EOF
 CACHE MANIFEST
 # version $version
 
+CACHE:
 EOF
 
-find . -name "*.js" -or -name "*.css" | sed "s/^\\./\/game/g" >> cache.manifest
+find . -name "*.js" -or -name "*.css" | sed "s/^\\./\\./g" >> $appcache
 
-echo "### cache.manifest ###"
-cat cache.manifest
+echo "### cache manifest ###"
+cat $appcache
 echo "######################"
 
 ################################
 # backup files
 ################################
 deploy_file="deploy_$version.tar.gz"
-tar cvzf $deploy_file --exclude="*/.*" index.html cache.manifest scripts/ styles/
+tar cvzf $deploy_file --exclude="*/.*" index.html $appcache scripts/ styles/
 
 ################################
 # upload
 ################################
 echo "Upload files to server..."
-scp $deploy_file jinius@ivorytower.co.kr:www/game/deploy.tar.gz
+scp $deploy_file $server:www/game/deploy.tar.gz
 
